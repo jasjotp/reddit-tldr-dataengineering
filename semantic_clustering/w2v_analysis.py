dbe3d5a1e0b9ac5ad9e67410388684e2f0814e9c
jasjotp <jasjotp@sfu.ca>
2025-04-05 18:12:21 -0700

    updated eda.py

--- semantic_clustering/w2v_analysis.py
+++ semantic_clustering/w2v_analysis.py
@@ -11,3 +11,11 @@ import os
 import sys 
 
 
+# add root directory of project to Python's import path so we can import modules from older folders
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from eda.eda import get_combined_data
+
+combined_df = get_combined_data()
+
+print(combined_df.head())
